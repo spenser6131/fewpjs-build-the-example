9c6bf1c1dd28f7bdd32bb3679490171c84b6f258
spenser6131 <brinkman.spenser@gmail.com>
2021-01-13 11:39:58 -0800

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const modal = document.getElementById("modal")
+const modalMessage = document.getElementById("modal-message")
+const hearts = document.getElementsByClassName("like-glyph")
 
-// Your JavaScript code goes here!
+let heartStates = {
+  '♡': '♥',
+  '♥': '♡'
+};
 
+let colorStates = {
+  "": "red",
+  "red" : ""
+};
+
+
+for(const heart of hearts) {
+  heart.addEventListener("click", likeFx)
+}
+
+function likeFx(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(){
+       heart.innerText = heartStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      modalMessage.innerText = error;
+      modal.className = "";
+      setTimeout(function(){
+        modal.className = "hidden"
+      }, 5000)
+    });
+}
 
 
 
